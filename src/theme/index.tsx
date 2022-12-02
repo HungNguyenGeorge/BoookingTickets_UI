f0066771aef8240abd2ad926f57b2798b3c158c1
Kio Nguyen <vanhungtbt1998@gmail.com>
2022-12-02 13:26:45 +0700

    Integrate Vite build

---
+++ src/theme/index.tsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { useMemo } from 'react';
+// @mui
+import { CssBaseline } from '@mui/material';
+import { Shadows, Palette, Theme  } from '@mui/material/styles';
+import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
+// import { Theme } from '@mui/material/styles/createTheme';
+//
+import palette from './palette';
+import shadows from './shadows';
+import typography from './typography';
+import GlobalStyles from './globalStyles';
+import customShadows from './customShadows';
+import componentsOverride from './overrides';
+import { Typography, TypographyOptions } from '@mui/material/styles/createTypography';
+
+// ----------------------------------------------------------------------
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default function ThemeProvider({ children }) {
+  const themeOptions = useMemo(
+    () => ({
+      palette: palette,
+      shape: { borderRadius: 6 },
+      typography: typography as TypographyOptions,
+      shadows: shadows() as Shadows,
+      customShadows: customShadows(),
+    }),
+    []
+  );
+  console.log("🚀 ~ file: index.tsx ~ line 34 ~ ThemeProvider ~ themeOptions", themeOptions)
+
+  const theme = createTheme(themeOptions);
+  theme.components = componentsOverride(theme);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <MUIThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        {children}
+      </MUIThemeProvider>
+    </StyledEngineProvider>
+  );
+}
